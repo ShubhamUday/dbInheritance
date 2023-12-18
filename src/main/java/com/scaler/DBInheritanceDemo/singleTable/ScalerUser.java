a63930cfb449217abbded43bc75d53474be71c14
shubhamuday <shubhamuday21@gmail.com>
2023-12-18 15:17:55 +0530

    checking how to push code

--- src/main/java/com/scaler/DBInheritanceDemo/singleTable/ScalerUser.java
+++ src/main/java/com/scaler/DBInheritanceDemo/singleTable/ScalerUser.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
-@Entity
+@Getter @Setter @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 public class ScalerUser {
     @Id
